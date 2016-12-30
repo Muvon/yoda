9554a960ea7eae101cec6b3343bbff4abaa15f76
Dmitry Kuzmenkov <dmitry@wagh.ru>
2016-12-31 00:58:49 +0700

    Add possibility of custom replacements for container templates

--- compose.sh
+++ compose.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+compose_container() {
+  if [[ -x "$COMPOSE_SCRIPT" ]]; then
+    cat - | $COMPOSE_SCRIPT --name=$1 --sequence=$2
+  else
+    cat -
+  fi
+}
+
 echo "# Build args $0 $*"
 echo 'version: "2"'
 echo 'networks:'
@@ -19,6 +27,6 @@ done
 
 for p in ${!SCALE_MAP[*]}; do
   for i in $(seq 0 ${SCALE_MAP[$p]:-0}); do
-    sed "s/^/  /g;s/#/$i/g" "$DOCKER_ROOT/containers/$p/container.yml"
+    sed "s/^/  /g;s/#/$i/g" "$DOCKER_ROOT/containers/$p/container.yml" | compose_container $p $i
   done
 done
--- help.sh
+++ help.sh
@@ -11,32 +11,33 @@ Commands available:
   compose   Display generated compose file for current environment
   start     Start all services for current project
   stop      Stop all services for current project
-echo
+
 Usage:
   $YODA_CMD version
     Display current Yoda version
-echo
+
   $YODA_CMD help
     Display help information
-echo
+
   $YODA_CMD init
     Prepare dockerized skeleton in project directory
-echo
+
   $YODA_CMD add [options]
     Add container skeleton to project
-echo
     Options are:
       --name=container – name of container (required)
-echo
+
   $YODA_CMD build
     Build images for current project
-echo
-  $YODA_CMD compose
+
+  $YODA_CMD compose [options]
     Display generated docker-compose file in stdout
-echo
+    Options are:
+      --composer=path-to-script – executable script who will process each container template, replace something and return as plain text. Container templates goes to stdin and 2 addition arguments are passed: --name and --sequence so name of container and number in scale map
+
   $YODA_CMD start [CONTAINER...]
     Start all containers or only passed with arguments
-echo
+
   $YODA_CMD stop [CONTAINER...]
     Stop all containers or only passed with arguments
 EOF
