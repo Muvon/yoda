586f8cc94eacdbb515240b3b36c000cd86f167ec
Dmitry Kuzmenkov <dmitry@wagh.ru>
2016-12-31 08:05:39 +0700

    Simplify env declaration without map

--- templates/env.sh
+++ templates/env.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-# Configure docker-compose
-declare -A ENV_MAP
-ENV_MAP=([dev]=dev [staging]=staging [prod]=prod [production]=prod)
-export ENV=${ENV_MAP[${ENV:-dev}]}
-
 # Any environment variables here
