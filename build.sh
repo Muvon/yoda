83c15da0b63be04430607ea3256d59948e2d1cf0
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-01-02 14:25:36 +0700

    Dont build image if it already exists. Closes #10

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 for line in "$(cat $DOCKER_ROOT/images/Buildfile)"; do
-  name=${line%%:*}
-  build_args=${line#*:}
-  docker build "$build_args" -f "$DOCKER_ROOT/images/Dockerfile-$name" .
+  image=$(eval echo $line | grep -Eo '\-t [^ ]+' | cut -d' ' -f2)
+  image_id=$(docker images -q $image)
+  if [[ -z "$image_id" ]]; then
+    name=${line%%:*}
+    build_args=${line#*:}
+    docker build $(eval echo $build_args) -f "$DOCKER_ROOT/images/Dockerfile-$name" .
+  else
+    echo "Image '$image' is already exist."
+  fi
 done
