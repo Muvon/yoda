abfc8ced189fe04391465b72ef038f4308421f13
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-02-06 15:51:05 +0700

    Fix service pass in yoda start command detection and image building. Closes #33

--- src/start.sh
+++ src/start.sh
@@ -27,5 +27,26 @@ fi
 $YODA_CMD compose > $COMPOSE_FILE
 # Get images we need to build
 images=$(grep image: $COMPOSE_FILE | sed 's|image:\(.*\)|\1|' | tr -d ' ' | sort | uniq)
-$YODA_CMD build ${build_args[*]} $images
-docker-compose up ${compose_args[*]} -t $STOP_WAIT_TIMEOUT -d $*
+
+images=()
+services=()
+for service in "$@"; do
+  image=$(grep image: $DOCKER_ROOT/containers/$service/container.yml | cut -d':' -f1 | tr -d ' ')
+  images+=($image)
+
+  service=$(cat $DOCKER_ROOT/Envfile | grep ^$ENV: | grep -oE "\b$service(=[0-9]+)?\b")
+
+  if [[ "$service" == *"="* ]]; then
+    count=$(echo $service | cut -d'=' -f2)
+    service=$(echo $service | cut -d'=' -f1)
+  else
+    count=1
+  fi
+
+  for n in $(seq 0 $((count - 1))); do
+    services+=($service.$n)
+  done
+done
+
+$YODA_CMD build ${build_args[*]} ${images[*]}
+docker-compose up ${compose_args[*]} -t $STOP_WAIT_TIMEOUT -d ${services[*]}
