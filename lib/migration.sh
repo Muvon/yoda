276d36e291298cb1004ca02606b495343075744f
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-04-20 17:54:54 +0700

    Fix version change on upgrade and deprecate old one function

--- lib/migration.sh
+++ lib/migration.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-change_version() {
-  old=$1
-  new=$2
-
-  if [[ -z "$old" || -z "$new" ]]; then
-    >&2 echo 'Usage: change_version "1.0" "1.1"'
-    return 1
-  fi
-
-  rcfile=$DOCKER_ROOT/.yodarc
-  cat $rcfile | sed 's|\(YODA_VERSION=\)"'$old'"|\1"'$new'"|g' > $rcfile.swp
-  mv -f $rcfile.swp $rcfile
-}
-
 update_yodarc() {
-  sed "s/{{name}}/$COMPOSE_PROJECT_NAME/g;s/{{yoda_version}}/$YODA_VERSION/g" $YODA_PATH/templates/yodarc > $DOCKER_ROOT/.yodarc
+  sed "s/{{name}}/$COMPOSE_PROJECT_NAME/g;s/{{yoda_version}}/$YODA_SOURCE_VERSION/g" $YODA_PATH/templates/yodarc > $DOCKER_ROOT/.yodarc
 
 }
