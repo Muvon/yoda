1a158b9017f9679e37c9d9b9fcaeedd749b4fe59
Dmitry Kuzmenkov <dmitry@wagh.ru>
2021-01-23 17:38:13 +0700

    Remove deprecated and unused code from build

--- src/build.sh
+++ src/build.sh
@@ -39,61 +39,55 @@ if [[ -f $lock_file && ! $force ]]; then
   exit 1
 fi
 
+if [[ ! $force ]]; then
+  trap unlock EXIT
+fi
+lock
+
+# Get images we should build
+declare -A images
+for image in "$@"; do
+  images[$image]=1
+done
+
 if [[ -n "$REGISTRY_URL" ]]; then
   echo "Using docker registry: $REGISTRY_URL"
 fi
 
-declare -A image_names
-declare -A image_ids
-declare -A image_build_args
-declare -A image_build_context
 mapfile -t lines < "$DOCKER_ROOT/Buildfile"
 for line in "${lines[@]}"; do
-  image=$(echo $line | grep -Eo '\-t [^ ]+' | cut -d' ' -f2)
-  image_id=$(eval echo $image)
-  name=${line%%:*}
-  args=$(eval echo ${line#*:})
+  image=$(echo "$line" | grep -Eo '\-t [^ ]+' | cut -d' ' -f2)
+  image_id=$(eval echo "$image")
 
-  [[ $name =~ ^([^\[]*)(\[(.*)\])?$ ]]
-  name=${BASH_REMATCH[1]}
-  context=${BASH_REMATCH[3]:-.}
-
-  image_ids[$name]=$image_id
-
-  extra_args=()
-  if [[ -n "$no_cache" ]]; then
-    extra_args+=('--no-cache')
+  echo -n "Image '$image_id' is "
+  if [[ -n "${images[*]}" && -z "${images[$build_image]}" && -z "${images[$build_id]}" ]]; then
+    echo 'skipped.'
+    continue
   fi
-  image_build_args[$name]="${extra_args[*]} $args"
-  image_build_context[$name]="$context"
-  image_names[$name]=$name
+
+  docker_image_id=$(docker images -q "$image_id")
+  if [[ -z "$docker_image_id" || -n "$rebuild" ]]; then
+    echo 'building.'
+
+    name=${line%%:*}
+    [[ $name =~ ^([^\[]*)(\[(.*)\])?$ ]]
+    name=${BASH_REMATCH[1]}
+    context=${BASH_REMATCH[3]:-.}
+
+    build_args=$(eval echo ${line#*:})
+    extra_args=()
+    if [[ -n "$no_cache" ]]; then
+      extra_args+=('--no-cache')
+    fi
+
+    docker build --network host ${extra_args[*]} $(eval echo "$build_args") -f "$DOCKER_ROOT/images/Dockerfile-$name" "$context"
+  else
+    echo 'built already.'
+  fi
+
+  # If we had setup REPOSITORY_URL and should push
+  if [[ -n "$push" && -n "$REGISTRY_URL" ]]; then
+    docker tag "$image_id" "$REGISTRY_URL/$image_id"
+    docker push "$REGISTRY_URL/$image_id"
+  fi
 done
-
-if [[ -z "$*" ]]; then
-  for name in "${image_names[@]}"; do
-    original_args+=" $name"
-  done
-  $YODA_CMD build $original_args
-else
-  if [[ ! $force ]]; then
-    trap unlock EXIT
-  fi
-  lock
-
-  for image_for_build in "$@"; do
-    echo -n "Image '${image_ids[$image_for_build]}' is "
-    docker_image_id=$(docker images -q "${image_ids[$image_for_build]}")
-    if [[ -z "$docker_image_id" || -n "$rebuild" ]]; then
-      echo 'building.'
-      docker build --network host "${image_build_args[$image_for_build]}" -f "$DOCKER_ROOT/images/Dockerfile-$image_for_build" "${image_build_context[$image_for_build]}"
-    else
-      echo 'built already.'
-    fi
-
-    # If we had setup REPOSITORY_URL and should push
-    if [[ -n "$push" && -n "$REGISTRY_URL" ]]; then
-      docker tag "${image_ids[$image_for_build]}" "$REGISTRY_URL/${image_ids[$image_for_build]}"
-      docker push "$REGISTRY_URL/${image_ids[$image_for_build]}"
-    fi
-  done
-fi
