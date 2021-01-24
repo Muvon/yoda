bca4c2a6f4cc997a895f046f033164ad33a773fe
Don Hardman <don.hardman@muvon.io>
2021-01-25 00:57:29 +0700

    Create docker/.ssh on init by default cuz we use it for setup

--- src/init.sh
+++ src/init.sh
@@ -21,7 +21,7 @@ username=$(git config --global user.name || echo 'Unknown maintainer')
 useremail=$(git config --global user.email || echo 'noreply@yoda.org')
 
 touch .dockerignore
-mkdir -p $yoda_dir/{images,containers}
+mkdir -p $yoda_dir/{images,containers,.ssh/connections}
 cp $YODA_PATH/templates/env.sh $yoda_dir
 cp $YODA_PATH/templates/{Env,Build,Start}file $yoda_dir
 sed "s/{{user}}/$username/g;s/{{email}}/$useremail/g;" $YODA_PATH/templates/Dockerfile > $yoda_dir/images/Dockerfile-base
