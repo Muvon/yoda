979b70bffa6f3fb69b353d424073f8ceabfcb03f
Don Hardman <don.hardman@muvon.io>
2021-01-30 17:26:25 +0700

    Add string lib  to manipulate with strings

---
+++ lib/string.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# shellcheck disable=SC1091 source=../lib/array.sh
+source "$YODA_PATH/lib/array.sh"
+
+# Usage string_replace [str?] [array of replaces]
+# Example:
+#  array=( 'from1/to1' 'from2/to2')
+#  echo "replace here" | string_replace "${array[@]}"
+#  string_replace "here we go" "${array[@]}"
+string_replace() {
+  local str
+  if [[ -p /dev/stdin ]]; then
+    str=$(cat -)
+  else
+    str=$1
+    shift
+  fi
+
+  local replaces=("$@")
+  echo "$str" | sed "s/$(array_join "/g;s/" "${replaces[@]}")/g"
+}
