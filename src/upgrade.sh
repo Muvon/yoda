bfebc39ce2a99894d063dab38d94bd7a48672845
Don Hardman <don.hardman@muvon.io>
2021-02-02 15:44:05 +0700

    Fix upgrade flow when we need to upgrade several versions up

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -6,10 +6,13 @@ if [[ -z "$upgrade_path" || ! -d "$upgrade_path" ]]; then
   exit 1
 fi
 
+# shellcheck source=../lib/migration.sh
+source "$YODA_PATH/lib/migration.sh"
+
 declare -A upgrades
 proj_major=${YODA_VERSION%.*}
 proj_minor=${YODA_VERSION#*.}
-for file in $upgrade_path/*; do
+for file in "$upgrade_path"/*; do
   file=${file##*/}
   version=${file#version-*}
   major=${version%.*}
@@ -18,9 +21,11 @@ for file in $upgrade_path/*; do
   if [[ $major -gt $proj_major || ( $major -eq $proj_major && $minor -gt $proj_minor ) ]]; then
     echo "Upgrade: $version"
     upgrades[$version]=1
-    exec $upgrade_path/$file
+    ( exec "$upgrade_path/$file" )
   fi
 done
-if [[ -z "${upgrades[@]}" ]]; then
+if [[ -n "${upgrades[*]}" ]]; then
+  update_yodarc
+else
   echo 'No upgrades found. You have the latest version in this project.'
 fi
